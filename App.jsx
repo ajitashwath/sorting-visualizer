3a76c1ca2c9c4f27b9cfb0a0e8e2fc1dba811763
{Ajit <ajitashwathr23@lpu.in>
2024-11-25 00:13:15 +0530

    added files

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import SortingVisualizer from './components/SortingVisualizer';
+
+function App() {
+  return (
+    <div className = "min-h-screen bg-gray-50">
+      <SortingVisualizer/>
+    </div>
+  );
+}
---
+++ src/utils/algorithm.js
@@ -0,0 +1,12 @@
+export const ALGORITHMS = {
+    bubble: {
+      name: 'Bubble Sort',
+      timeComplexity: 'O(n²)',
+      spaceComplexity: 'O(1)',
+      description: 'Repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order.',
+      bestCase: 'O(n)',
+      averageCase: 'O(n²)',
+      worstCase: 'O(n²)'
+    },
+    // will fucking add
+};
