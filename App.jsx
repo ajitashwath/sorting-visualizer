9db37392e9038334d83327abb23a7d990b573533
{Ajit <ajitashwathr23@lpu.in>
2024-11-25 00:17:52 +0530

    added files

--- App.jsx
+++
@@ -1,9 +0,0 @@
-import SortingVisualizer from './components/SortingVisualizer';
-
-function App() {
-  return (
-    <div className = "min-h-screen bg-gray-50">
-      <SortingVisualizer/>
-    </div>
-  );
-}
--- src/utils/constant.js
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_ARRAY_SIZE = 50;
-export const DEFAULT_SPEED = 50;
-export const MIN_ARRAY_SIZE = 10;
-export const MAX_ARRAY_SIZE = 100;
-export const MIN_VALUE = 10;
-export const MAX_VALUE = 300;
